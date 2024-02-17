a97ca6f3753967a554aa724f378feb4d1afed393
Aadesh Bajaj <aadeshba@usc.edu>
2024-02-17 15:46:55 -0800

    changed comment, nothing really changed here ngl

--- app/api/v1/external_apis/stripe_api.py
+++ app/api/v1/external_apis/stripe_api.py
@@ -1,6 +1,5 @@
 from base_api import BaseApi
 from dotenv import load_dotenv
-from pathlib import Path
 import os                                                                                                                                                                                                          
 
 
@@ -24,14 +23,15 @@ class StripeAPI(BaseApi):
             # donations = stripe.
             # print(len(donations))
             charges = stripe.Charge.list(
-                created={
-                    "gte": start_date,  # greater than or equal to timestamp for one week ago
-                    "lt": end_date    # less than timestamp til now (~feb 17th 2023 2:10:00)
+                created = {
+                    "gte": start_date,  # greater than or equal to this timestamp (start time)
+                    "lt": end_date    # less than this timestamp (end time)
                 }
             )
             
             # Iterate through the charges and do something with them
             for charge in charges.auto_paging_iter():
+                # print(charge)
                 print(f"Name: {charge['billing_details']['name']}, "
                     f"Amount: {charge['amount']}, "
                     f"Currency: {charge['currency']}, "
