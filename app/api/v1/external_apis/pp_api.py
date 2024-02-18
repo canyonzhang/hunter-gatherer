ef5d78d54d9f6f72d283b4a5723eec7b0b5e7d2f
Mantej Lamba <mlamba@usc.edu>
2024-02-17 16:32:15 -0800

    donations filtering

--- app/api/v1/external_apis/pp_api.py
+++ app/api/v1/external_apis/pp_api.py
@@ -11,11 +11,10 @@ class PayPalAPI(BaseApi):
         self.headers = {
         }
 
-
-
     def get_oauth_token(self):
         auth_data = {"grant_type": "client_credentials"}
-        auth_response = requests.post(self.base_url + "/v1/oauth2/token", auth=(self.client_id, self.client_secret), data=auth_data, headers=self.headers)
+        auth_response = requests.post(self.base_url + "/v1/oauth2/token", auth=(self.client_id, self.client_secret),
+                                      data=auth_data, headers=self.headers)
         json = auth_response.json()
         print(json)
         print()
@@ -25,15 +24,33 @@ class PayPalAPI(BaseApi):
             auth_response.raise_for_status()
 
     def search_transactions(self, start_date: str, end_date: str, transaction_type: str = None):
-        #do this every time
+        # do this every time
         self.get_oauth_token()
         params = {"start_date": start_date, "end_date": end_date}
         if transaction_type:
             params['transaction_type'] = transaction_type
-        #mak sure to only put in non base part of the url when call make_request
+        # mak sure to only put in non base part of the url when call make_request
         return self.make_request("/v1/reporting/transactions", "GET", headers=self.headers, params=params)
-        
+
+    def get_donations(self, start_date: str, end_date: str):
+        self.get_oauth_token()
+        total_transactions = self.search_transactions(start_date, end_date)
+        if total_transactions:
+            donation_orders = [transaction for transaction in total_transactions['transaction_details'] if
+                               'transaction_info' in transaction and
+                               'transaction_subject' in transaction['transaction_info'] and
+                               'Donation' in transaction['transaction_info']['transaction_subject']]
+
+            return donation_orders
+        else:
+            print("Error retrieving transactions.")
+            return None
+
+
 # Example call
-paypal_api = PayPalAPI('AcDpeX9nbG4qRF3NXXhq4PcrtlP7-0fbF7XBSaMxpboQpQStViOMKMlqVGfJ0wrSDWg71_Zp1w06hFY_', 'EITIFRLCoS05rQd5cqDRkPkjjduQZ4AHOWkPFHwSjN-D8L2CgsQ6nCcbzsrEt40BvVTBFZSzcRGtGB8P')
-transactions = paypal_api.search_transactions('2023-07-01T00:00:00-0700', '2023-07-03T00:00:00-0700')
-print(transactions['transaction-details'])
+paypal_api = PayPalAPI('AcDpeX9nbG4qRF3NXXhq4PcrtlP7-0fbF7XBSaMxpboQpQStViOMKMlqVGfJ0wrSDWg71_Zp1w06hFY_',
+                       'EITIFRLCoS05rQd5cqDRkPkjjduQZ4AHOWkPFHwSjN-D8L2CgsQ6nCcbzsrEt40BvVTBFZSzcRGtGB8P')
+transactions = paypal_api.search_transactions('2023-10-01T00:00:00-0700', '2023-10-31T00:00:00-0700')
+donations = paypal_api.get_donations('2023-12-01T00:00:00-0700', '2023-12-30T00:00:00-0700')
+print(donations)
+print(transactions['transaction_details'])
--- app/api/v1/external_apis/stripe_api.py
+++ app/api/v1/external_apis/stripe_api.py
@@ -11,17 +11,17 @@ class StripeAPI(BaseApi):
         super().__init__("https://api.stripe.com")
         self.headers = {
         }
-    
+
     def search_transactions(self, start_date, end_date):
         try:
             # List charges for a specific customer and optionally within a certain time range
             charges = stripe.Charge.list(
                 created={
                     "gte": start_date,  # timestamp for one week ago
-                    "lt": end_date    # timestamp for now (feb 17th 2023 2:10:00)
+                    "lt": end_date  # timestamp for now (feb 17th 2023 2:10:00)
                 }
             )
-            
+
             # Iterate through the charges and do something with them
             for charge in charges.auto_paging_iter():
                 print(charge)
@@ -29,6 +29,36 @@ class StripeAPI(BaseApi):
             # Handle error
             print(e)
 
+    def get_donations(self, start_date, end_date):
+        try:
+            total_donations = []
+            charges = stripe.Charge.list(
+                created={
+                    "gte": start_date,
+                    "lt": end_date
+                }
+            )
+
+            for charge in charges.auto_paging_iter():
+                if 'Donation' in charge.get('description'):
+                    total_donations.append(charge)
+
+            return total_donations
+        except stripe.error.StripeError as e:
+            print(e)
+            return None
+
 
 stripe_api = StripeAPI()
 stripe_api.search_transactions(1707602577, 1708207377)
+
+donations = stripe_api.get_donations(1672614308, 1704063908)
+
+if donations:
+    for donation in donations:
+        print(f"Donation Transaction ID: {donation['id']}")
+        print(f"Amount: {donation['amount'] / 100} {donation['currency']}")
+        print(f"Description: {donation['description']}")
+        print("-------------")
+else:
+    print("No donations found.")
